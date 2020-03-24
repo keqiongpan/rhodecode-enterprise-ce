b63ea231e59587abc97caee17734119cd030e829
Marcin Kuzminski <marcin@rhodecode.com>
2020-03-24 10:48:29 +0000

    release: updated pip2nix output for 4.18.3

--- pkgs/python-packages.nix
+++ pkgs/python-packages.nix
@@ -1779,7 +1779,7 @@ self: super: {
     };
   };
   "rhodecode-enterprise-ce" = super.buildPythonPackage {
-    name = "rhodecode-enterprise-ce-4.18.2";
+    name = "rhodecode-enterprise-ce-4.18.3";
     buildInputs = [
       self."pytest"
       self."py"
