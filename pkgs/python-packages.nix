73104ef089a7703b0a17269551d11f1e9583b776
Milka Kuzminski <milka@rhodecode.com>
2020-11-23 09:13:16 +0100

    core: bump nix packages version

--- pkgs/python-packages.nix
+++ pkgs/python-packages.nix
@@ -1883,7 +1883,7 @@ self: super: {
     };
   };
   "rhodecode-enterprise-ce" = super.buildPythonPackage {
-    name = "rhodecode-enterprise-ce-4.22.0";
+    name = "rhodecode-enterprise-ce-4.23.0";
     buildInputs = [
       self."pytest"
       self."py"
