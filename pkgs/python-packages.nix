364bc6bfc18d4be384b360634c490c0ba9a0f553
Milka Kuzminski <milka@rhodecode.com>
2021-03-09 21:16:40 +0100

    release: Bump version 4.24.0 to 4.25.0

--- pkgs/python-packages.nix
+++ pkgs/python-packages.nix
@@ -1883,7 +1883,7 @@ self: super: {
     };
   };
   "rhodecode-enterprise-ce" = super.buildPythonPackage {
-    name = "rhodecode-enterprise-ce-4.24.0";
+    name = "rhodecode-enterprise-ce-4.25.0";
     buildInputs = [
       self."pytest"
       self."py"
